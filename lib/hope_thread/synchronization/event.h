88b0491329ae88c826cd97b96279acd09f53eade
glensand <bezborodoff.gleb@gmail.com>
2024-06-25 05:06:40 -0700

    [event] fixed wrong method call

--- lib/hope_thread/synchronization/event.h
+++ lib/hope_thread/synchronization/event.h
@@ -22,7 +22,8 @@
 
 namespace hope::threading {
 
-    class synchronization_event {
+#if defined(_WIN32) || defined(_WIN64)
+ class synchronization_event {
     protected:
         enum class policy : unsigned {
             Auto,
@@ -30,52 +31,65 @@ namespace hope::threading {
         };
 
         explicit synchronization_event(policy policy) noexcept {
-#if defined(_WIN32) || defined(_WIN64)
             m_event = CreateEvent(nullptr, policy != policy::Auto,
                                   false, nullptr);
-#endif
         }
     public:
         HOPE_THREADING_CONSTRUCTABLE_ONLY(synchronization_event);
         virtual ~synchronization_event() noexcept {
-#if defined(_WIN32) || defined(_WIN64)
             CloseHandle(m_event);
-#endif
         };
 
         void set() const noexcept {
-#if defined(_WIN32) || defined(_WIN64)
             SetEvent(m_event);
-#else
-            m_cv.notify_one();
-#endif
         }
 
         void reset() const noexcept{
-#if defined(_WIN32) || defined(_WIN64)
             ResetEvent(m_event);
-#endif
         }
 
         bool wait(long waiting_time = std::numeric_limits<long>::max()) const noexcept {
-#if defined(_WIN32) || defined(_WIN64)
             return  WaitForSingleObject(m_event, waiting_time) != WAIT_TIMEOUT;
-#else
-            std::unique_lock lk(m_mutex);
-            m_cv.wait_for(lk, std::chrono::microseconds(waiting_time));
-            return true;
-#endif
         }
 
     private:
-#if defined(_WIN32) || defined(_WIN64)
         using handle = void*;
         handle m_event;
+    };
 #else
+ class synchronization_event {
+    protected:
+        enum class policy : unsigned {
+            Auto,
+            Manual,
+        };
+
+        explicit synchronization_event(policy policy) noexcept {
+        }
+    public:
+        HOPE_THREADING_CONSTRUCTABLE_ONLY(synchronization_event);
+        virtual ~synchronization_event() noexcept {
+        };
+
+        void set() const noexcept {
+            m_cv.notify_one();
+        }
+
+        void reset() const noexcept{
+
+        }
+
+        bool wait(long waiting_time = std::numeric_limits<long>::max()) const noexcept {
+            std::unique_lock lk(m_mutex);
+            m_cv.wait_until(lk, std::chrono::system_clock::now() + std::chrono::microseconds(waiting_time));
+            return true;
+        }
+
+    private:
         mutable std::condition_variable m_cv;
         mutable std::mutex m_mutex;
-#endif
     };
+#endif
 
     class auto_reset_event final : public synchronization_event {
     public:
